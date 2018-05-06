f9facdf8d1de6d1ecad6f2da137e6ea79ec05b6e
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-05-06 19:57:58 -0300

    teste para o nome real. Fala colocar o nome real adequadamente :/

--- tests/test_gamb.py
+++ tests/test_gamb.py
@@ -1,20 +1,25 @@
 import unittest
-import os
+from os import system as sys
 from gambs.valor1.valor1 import ator_from_url
 
 class TesteAtor(unittest.TestCase):
 	def teste_nome(self):
 		usuario=ator_from_url('https://www.instagram.com/oimundoembr/')
 		self.assertEqual('oimundoembr', usuario.nome)
-		os.system("rm *.html")
-
+		sys("rm *.html")
+		
 	def teste_seguidores(self):
 		usuario=ator_from_url('https://www.instagram.com/oimundoembr/')
 		self.assertEqual(0, usuario.seg)
-		os.system("rm *.html")
+		sys("rm *.html")
+
 
 	def teste_postagens(self):
 		usuario=ator_from_url('https://www.instagram.com/oimundoembr/')
 		self.assertEqual(0, usuario.post)
-		os.system("rm *.html")
+		sys("rm *.html")
 
+	def teste_nome_real(self):
+		usuario=ator_from_url('https://www.instagram.com/oimundoembr/')
+		self.assertEqual('instagram instagram', usuario.real)
+		sys("rm *.html")
